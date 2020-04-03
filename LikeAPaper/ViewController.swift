8c97df44f843a6e65cec1e8ee4c5e5c6284b04d4
nakajima <symn43@gmail.com>
2020-04-03 16:00:20 +0900

    Initial Commit

---
+++ LikeAPaper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LikeAPaper
+//
+//  Created by nakajima on 2020/04/03.
+//  Copyright © 2020 Tsuyoshi nakajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
