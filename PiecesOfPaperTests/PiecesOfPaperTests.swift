588af7e06a19fd5e87b7493cda72178ad0938df5
nakajima <symn43@gmail.com>
2021-01-15 15:05:17 +0900

    Re-created all test resources

---
+++ PiecesOfPaperTests/PiecesOfPaperTests.swift
@@ -0,0 +1,29 @@
+//
+//  PiecesOfPaperTests.swift
+//  PiecesOfPaperTests
+//
+//  Created by nakajima on 2021/01/15.
+//  Copyright © 2021 Tsuyoshi nakajima. All rights reserved.
+//
+
+import XCTest
+@testable import Pieces_of_Paper
+
+class PiecesOfPaperTests: XCTestCase {
+
+    //    func test_saveButton_status_is_Disabled() throws {
+    //        let storyboard = UIStoryboard(name: "ThumbnailCollectionViewController", bundle: nil)
+    //        let viewController = storyboard.instantiateViewController(withIdentifier: "ThumbnailCollectionViewController") as? ThumbnailCollectionViewController
+    //
+    //        guard let isEnable = viewController?.autosaveButton.isEnabled else {
+    //            XCTFail("ThumbnailCollectionViewController is nil")
+    //            return
+    //        }
+            
+            // Okay, autosaveButton would be nil. I never call the initializer of collectionView
+    //        let viewController = ThumbnailCollectionViewController()
+    //        XCTAssertFalse(viewController.autosaveButton.isEnabled,
+    //                       "saveButton status is Disabled before document open")
+    //    }
+
+}
