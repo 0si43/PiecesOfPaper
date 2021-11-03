e4216d15d83638afa3e7d5bdb6bb6cd954069d3a
st43 <45909001+0si43@users.noreply.github.com>
2021-11-03 14:10:22 +0900

    Change app lunch to SwiftUI View

---
+++ PiecesOfPaper/PiecesOfPaperApp.swift
@@ -0,0 +1,18 @@
+//
+//  PiecesOfPaperApp.swift
+//  PiecesOfPaper
+//
+//  Created by nakajima on 2021/11/03.
+//  Copyright © 2021 Tsuyoshi nakajima. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PiecesOfPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NotesGrid()
+        }
+    }
+}
