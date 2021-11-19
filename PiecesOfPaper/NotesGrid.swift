7864aa7977d32867e0d5457163ad1aea0cfdc72a
st43 <45909001+0si43@users.noreply.github.com>
2021-11-19 12:25:10 +0900

    Make NavigationView

--- PiecesOfPaper/NotesGrid.swift
+++ PiecesOfPaper/NotesGrid.swift
@@ -36,6 +36,7 @@ struct NotesGrid: View {
                     Image(systemName: "plus")
                 }
             )
+            .navigationBarTitleDisplayMode(.inline)
             .overlay(
                 VStack {
                     Spacer()
--- PiecesOfPaper/PiecesOfPaperApp.swift
+++ PiecesOfPaper/PiecesOfPaperApp.swift
@@ -12,7 +12,35 @@ import SwiftUI
 struct PiecesOfPaperApp: App {
     var body: some Scene {
         WindowGroup {
-            NotesGrid()
+            NavigationView {
+                List {
+                    Section(header: Text("Folder")) {
+                        NavigationLink(destination: NotesGrid()) {
+                            Label("Home", systemImage: "tray")
+                        }
+                        NavigationLink(destination: EmptyView()) {
+                            Label("All Notes", systemImage: "tray.full")
+                        }
+                        NavigationLink(destination: EmptyView()) {
+                            Label("Archived", systemImage: "archivebox")
+                        }
+                    }
+                    Section(header: Text("Setting")) {
+                        NavigationLink(destination: EmptyView()) {
+                            Label("Setting", systemImage: "gearshape")
+                        }
+                    }
+                    Section(header: Text("About")) {
+                        NavigationLink(destination: EmptyView()) {
+                            Label("Github Repository", systemImage: "wrench")
+                        }
+                        NavigationLink(destination: EmptyView()) {
+                            Label("Developer Site", systemImage: "wrench")
+                        }
+                    }
+                }
+                .navigationTitle("Pieces of Paper")
+            }
         }
     }
 }
