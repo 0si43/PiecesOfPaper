a0b123dbc3f1863aa55af27cdb31f7e1e73ec051
st43 <45909001+0si43@users.noreply.github.com>
2021-11-27 12:40:09 +0900

    add actions contextMenu

--- PiecesOfPaper/View/NotesGrid.swift
+++ PiecesOfPaper/View/NotesGrid.swift
@@ -10,8 +10,21 @@ import SwiftUI
 import PencilKit
 
 struct NotesGrid: View {
+    @State var isShowActivityView = false
+    @State var documentToShare: NoteDocument?
     @Binding var noteDocuments: [NoteDocument]
     let gridItem = GridItem(.adaptive(minimum: 250), spacing: 50.0)
+    var activityViewController: UIActivityViewControllerWrapper? {
+        guard let document = documentToShare else { return nil }
+        let drawing = document.drawing
+        var image = UIImage()
+        let trait = UITraitCollection(userInterfaceStyle: .light)
+        trait.performAsCurrent {
+            image = drawing.image(from: drawing.bounds, scale: UIScreen.main.scale)
+        }
+        
+        return UIActivityViewControllerWrapper(activityItems: [image])
+    }
     
     var body: some View {
         LazyVGrid(columns: [gridItem], spacing: 60.0) {
@@ -25,30 +38,53 @@ struct NotesGrid: View {
                         .shadow(radius: 5.0)
                 }
                 .contextMenu {
-                    Button(action: { print("temp") }) {
+                    Button(action: {
+                        duplicate(noteDocument: noteDocuments[index])
+                    }) {
                         Label("Duplicate", systemImage: "doc.on.doc")
                     }
                     if #available(iOS 15.0, *) {
                         Button(role: .destructive) {
-                            print("temp")
+                            delete(noteDocument: noteDocuments[index])
                         } label: {
                             Label("Delete", systemImage: "trash")
                         }
                     } else {
-                        Button(action: { print("temp") }) {
+                        Button(action: {
+                            delete(noteDocument: noteDocuments[index])
+                        }) {
                             Label("Delete", systemImage: "trash")
                         }
                     }
-                    Button(action: { print("temp") }) {
+                    Button(action: {
+                        share(noteDocument: noteDocuments[index])
+                    }) {
                         Label("Share", systemImage: "square.and.arrow.up")
                     }
                 }
             }
         }
+        .sheet(isPresented: $isShowActivityView) {
+            activityViewController
+        }
     }
     
     func open(noteDocument: NoteDocument) {
         Router.shared.openCanvas(noteDocument: noteDocument)
+    }
+    
+    func duplicate(noteDocument: NoteDocument) {
+        let newUrl = FilePath.iCloudURL.appendingPathComponent(FilePath.fileName)
+        try? FileManager.default.copyItem(at: noteDocument.fileURL, to: newUrl)
+    }
+    
+    func delete(noteDocument: NoteDocument) {
+        try? FileManager.default.removeItem(at: noteDocument.fileURL)
+    }
+    
+    func share(noteDocument: NoteDocument) {
+        documentToShare = noteDocument
+        isShowActivityView = true
     }
 }
 
