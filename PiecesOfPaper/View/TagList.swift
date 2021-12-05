37351ebcb2924104c434c5bc9e8c22ec8d36ec50
0si43 <symn43@gmail.com>
2021-12-05 23:50:52 +0900

    added edit mode and deleted  document variant on App file

--- PiecesOfPaper/View/TagList.swift
+++ PiecesOfPaper/View/TagList.swift
@@ -10,29 +10,36 @@ import SwiftUI
 
 struct TagList: View {
     @ObservedObject var tagListViewModel = TagListViewModel()
-//    var noteDocument: NoteDocument?
+    @Environment(\.editMode) var editMode
 
     var body: some View {
-        List(tagListViewModel.tags) { tag in
-            Text(tag.name)
-                .padding(.vertical, 4)
-                .padding(.horizontal, 8)
-                .background(tag.color.swiftUIColor)
-                .cornerRadius(4)
-                .onTapGesture {
-                    guard let noteDocument = TagListRouter.shared.documentForPass else { return }
-                    if noteDocument.entity.tags.contains(tag.name) {
-                        noteDocument.entity.tags = Array(noteDocument.entity.tags.drop { $0 == tag.name })
-                    } else {
-                        noteDocument.entity.tags.append(tag.name)
-                    }
-                    noteDocument.save(to: noteDocument.fileURL, for: .forOverwriting) { success in
-                        if !success {
-                            print("save failed")
+        List {
+            ForEach(tagListViewModel.tags, id: \.id) { tag in
+                Text(tag.name)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .background(tag.color.swiftUIColor)
+                    .cornerRadius(4)
+                    .onTapGesture {
+                        guard let noteDocument = TagListRouter.shared.documentForPass else { return }
+                        if noteDocument.entity.tags.contains(tag.name) {
+                            noteDocument.entity.tags = Array(noteDocument.entity.tags.drop { $0 == tag.name })
+                        } else {
+                            noteDocument.entity.tags.append(tag.name)
+                        }
+                        noteDocument.save(to: noteDocument.fileURL, for: .forOverwriting) { success in
+                            if !success {
+                                print("save failed")
+                            }
                         }
                     }
-                }
+            }
+            .onDelete { _ in print("delete") }
         }
+        .navigationBarItems(trailing: EditButton())
+    }
+
+    private func addTag(to document: NoteDocument) {
 
     }
 }
